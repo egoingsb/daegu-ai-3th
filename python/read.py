76e1d99c8c4876726e02b2a6cfac51fc56ac8347
Glitch (glitch-hello-website) <none>
2022-04-18 11:16:07 +0000

    🏬🍁 Checkpoint ./python/read.py:3868946/249

---
+++ python/read.py
@@ -0,0 +1,7 @@
+import sqlite3
+conn = sqlite3.connect('db.sqlite3')
+cursor = conn.cursor()
+id = input('id ? ')
+cursor.execute('SELECT * FROM topics WHERE id = ?', (id,))
+topics = cursor.fetchall()
+print(topics);
