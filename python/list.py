dfae0d79146d4dfe5fad7df02c577073ed50f626
Glitch (glitch-hello-website) <none>
2022-04-18 08:54:09 +0000

    👟🙈 Checkpoint ./python/list.py:3868946/217 ./python/read.py:3868946/377 ./script.js:3868946/1841

--- python/list.py
+++ python/list.py
@@ -1,4 +1,8 @@
 import sqlite3
-con = sqliet3.connect('example.db')
+con = sqlite3.connect('db.sqlite3')
 cur = con.cursor()
-cur.
+cur.execute('SELECT * FROM topics')
+topics = cur.fetchall()
+for id, title, body in topics:
+  print(id, title, body)
+cur.close()
