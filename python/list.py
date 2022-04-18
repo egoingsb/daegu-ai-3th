96fa7c0911510f1a8a3ce57bb7a9be6e66bead94
Glitch (glitch-hello-website) <none>
2022-04-18 09:44:16 +0000

    불필요한 파일 제거

--- python/list.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-con = sqlite3.connect('db.sqlite3')
-cur = con.cursor()
-cur.execute('SELECT * FROM topics')
-topics = cur.fetchall()
-for id, title, body in topics:
-  print(id, title, body)
-cur.close()
--- python/read.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('db.sqlite3')
-cur = conn.cursor()
-id = input('id? ')
-cur.execute('SELECT * FROM topics WHERE id = ?', [id])
-topic = cur.fetchone()
-print('id:', topic[0], 'title:', topic[1], 'body:', topic[2])
