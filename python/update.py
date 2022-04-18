c766dc3583f357f54ea0f858d49282951074f310
Glitch (glitch-hello-website) <none>
2022-04-18 13:01:08 +0000

    update

--- python/update.py
+++ python/update.py
@@ -6,3 +6,11 @@ cur.execute('SELECT * FROM topics WHERE id=?', (id,))
 topic=cur.fetchone()
 title = input(f'title? ({topic[1]})')
 body = input(f'body? ({topic[2]})')
+
+cur.execute('UPDATE topics SET title=?, body=? WHERE id=?', (title, body, id))
+conn.commit()
+
+cur.execute('SELECT * FROM topics WHERE id=?', (id, ))
+topic = cur.fetchone()
+print('id:', topic[0], 'title:', topic[1], 'body:', topic[2])
+conn.close()
