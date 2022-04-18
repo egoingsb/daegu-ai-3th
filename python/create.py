91fabebc9623c11a5fd98f6f59ca3d7f9d6c659c
Glitch (glitch-hello-website) <none>
2022-04-18 11:01:23 +0000

    create

--- python/create.py
+++ python/create.py
@@ -1,4 +1,8 @@
 import sqlite3
 conn = sqlite3.connect('db.sqlite3')
 cur = conn.cursor()
-cur.execute('INSERT INTO ')
+title = input('title? ')
+body = input('body? ')
+cur.execute('INSERT INTO topics (title, body) VALUES(?, ?)', (title, body))
+conn.commit()
+conn.close()
