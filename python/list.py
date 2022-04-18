7d09822bcdd7e9746a025bd1b72f7c846733a107
Glitch (glitch-hello-website) <none>
2022-04-18 11:56:38 +0000

    list를 보기 좋게 개선

--- python/list.py
+++ python/list.py
@@ -2,6 +2,8 @@ import sqlite3
 conn = sqlite3.connect('db.sqlite3')
 cur = conn.cursor()
 cur.execute('SELECT * FROM topics')
-topics = cur.fetchone()
+topics = cur.fetchall()
 print(topics)
+for topic in topics:
+  print('id:',topic[0],' title:',topic[1])
 conn.close()
